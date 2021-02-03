1b770a71fda0118d84a66fd0546b76599af783d6
Adam Salehe Mtitima <adamsalehe31@gmail.com>
2021-02-03 21:13:12 +0300

    Adding Notifications

--- src/components/chatScreen.js
+++ src/components/chatScreen.js
@@ -3,10 +3,11 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPaperPlane, faMicrophone } from "@fortawesome/free-solid-svg-icons";
 import useEventListener from "../utils/useEventListener";
 import { applyColor } from "../utils/colors";
-import './chatScreen.css';
+import "./chatScreen.css";
+import sendNotification from "../utils/notify";
 
 const ChatScreen = props => {
-  const user  = sessionStorage.getItem("user");
+  const user = sessionStorage.getItem("user");
   const [isFull, setFull] = useState(false);
 
   let form;
@@ -14,20 +15,19 @@ const ChatScreen = props => {
 
   useEffect(() => {
     props.socket.on("chat:new", data => {
-      document.querySelector("#feedback").innerText = '';
+      document.querySelector("#feedback").innerText = "";
       newMessage(data);
-      applyColor(document.querySelectorAll('#received .message'));
-      applyColor(document.querySelectorAll('#sent .message'));
+      applyColor(document.querySelectorAll("#received .message"));
+      applyColor(document.querySelectorAll("#sent .message"));
     });
     props.socket.on("user:typing", data => {
       document.querySelector("#feedback").innerText = `${data.user} is typing..`;
     });
   }, []);
-  
 
   useEffect(() => {
-    applyColor(document.querySelectorAll('#received .message'));
-    applyColor(document.querySelectorAll('#sent .message'));
+    applyColor(document.querySelectorAll("#received .message"));
+    applyColor(document.querySelectorAll("#sent .message"));
     messagesElement = document.querySelector("#messages");
     messagesElement.scrollTo({
       behavior: "smooth",
@@ -36,14 +36,25 @@ const ChatScreen = props => {
   }, []);
 
   const newMessage = data => {
+    let reply = sendNotification(data);
+    console.log(reply);
     messagesElement = document.querySelector("#messages");
-  
+
     let div = document.createElement("div");
     div.classList.add("w-75", "my-1", user === data.user ? "ml-auto" : "mr-auto");
     div.id = user === data.user ? "sent" : "recieved";
 
     let div2 = document.createElement("div");
-    div2.classList.add("card", "d-flex", "flex-row", "bg-transparent", "border-0", "w-100", "p-2", "h-100");
+    div2.classList.add(
+      "card",
+      "d-flex",
+      "flex-row",
+      "bg-transparent",
+      "border-0",
+      "w-100",
+      "p-2",
+      "h-100"
+    );
 
     let div3 = document.createElement("div");
     div3.classList.add("message", "card-body", "rounded", "py-2", "w-100", "overflow-hidden");
@@ -64,32 +75,40 @@ const ChatScreen = props => {
     div2.appendChild(div3);
     div.appendChild(div2);
     messagesElement.appendChild(div);
-  
+
     messagesElement.scrollTo({
       behavior: "smooth",
       top: document.body.offsetHeight,
     });
   };
 
-  useEventListener("submit", event => {
-    event.preventDefault();
-    form = document.querySelector("#chatform");
-    let message = form["chat-text"].value;
-    if (message.toString().trim() === "") return false;
-    props.socket.emit("chat", {
-      room: props.room,
-      user,
-      message
-    });
-    form.reset();
-  }, document.querySelector("#chatform"));
+  useEventListener(
+    "submit",
+    event => {
+      event.preventDefault();
+      form = document.querySelector("#chatform");
+      let message = form["chat-text"].value;
+      if (message.toString().trim() === "") return false;
+      props.socket.emit("chat", {
+        room: props.room,
+        user,
+        message,
+      });
+      form.reset();
+    },
+    document.querySelector("#chatform")
+  );
 
-  useEventListener("keypress", () => {
-    props.socket.emit("chat:typing", {
-      room: props.room,
-      user
-    });
-  }, document.querySelector("#chat-text"));
+  useEventListener(
+    "keypress",
+    () => {
+      props.socket.emit("chat:typing", {
+        room: props.room,
+        user,
+      });
+    },
+    document.querySelector("#chat-text")
+  );
 
   const userList = props.users.map((user, index) => {
     // bg-warning, bg-danger
@@ -102,32 +121,32 @@ const ChatScreen = props => {
   });
 
   return (
-      <div id="container" className="text-white w-100">
-        <div className="d-flex flex-column w-75 h-100 float-left rounded-left">
-          <div id="messages" className="d-flex flex-column flex-grow-1 w-100">
-            <span id="feedback" className="mx-3 mt-1 text-muted text-monospace font-italic"></span>
-          </div>
-          <form id="chatform" className="w-100 d-flex flex-row flex-nowrap form-inline">
-            <input
-              type="text"
-              className="w-100 form-control form-control-lg text-white border-0 rounded-0"
-              id="chat-text"
-              name="chat-text"
-              placeholder="Text Message"
-            />
-            <button className="btn btn-lg btn-info rounded-0">
-              <FontAwesomeIcon icon={faMicrophone} />
-            </button>
-            <button type="submit" className="btn btn-lg btn-success rounded-0">
-              <FontAwesomeIcon icon={faPaperPlane} />
-            </button>
-          </form>
+    <div id="container" className="text-white w-100">
+      <div className="d-flex flex-column w-75 h-100 float-left rounded-left">
+        <div id="messages" className="d-flex flex-column flex-grow-1 w-100">
+          <span id="feedback" className="mx-3 mt-1 text-muted text-monospace font-italic"></span>
         </div>
-        <div id="user-list" className="h-100 w-25 float-right rounded-right">
-          <p className="text-center m-0 p-3">Users Online</p>
-          {userList}
-        </div>
+        <form id="chatform" className="w-100 d-flex flex-row flex-nowrap form-inline">
+          <input
+            type="text"
+            className="w-100 form-control form-control-lg text-white border-0 rounded-0"
+            id="chat-text"
+            name="chat-text"
+            placeholder="Text Message"
+          />
+          <button className="btn btn-lg btn-info rounded-0">
+            <FontAwesomeIcon icon={faMicrophone} />
+          </button>
+          <button type="submit" className="btn btn-lg btn-success rounded-0">
+            <FontAwesomeIcon icon={faPaperPlane} />
+          </button>
+        </form>
       </div>
+      <div id="user-list" className="h-100 w-25 float-right rounded-right">
+        <p className="text-center m-0 p-3">Users Online</p>
+        {userList}
+      </div>
+    </div>
   );
 };
 
