b17289b9b6a7e9917a9b2e160695139ef76a39fd
AnoRebel <hacker4rebel@gmail.com>
2022-11-24 23:18:08 +0300

    Changelog: - Fixed import/export typo

--- src/components/index.js
+++ src/components/index.js
@@ -4,5 +4,6 @@ import Loading from "@/components/Loading.vue";
 import ScrollToTop from "@/components/ScrollToTop.vue";
 import SideBar from "@/components/SideBar.vue";
 import SideLink from "@/components/SideLink.vue";
+import ToolBar from "@/components/ToolBar.vue";
 
-export { Footer, Header, Loading, ScrollToTop, SideBar, SideLink };
+export { Footer, Header, Loading, ScrollToTop, SideBar, SideLink, ToolBar };
